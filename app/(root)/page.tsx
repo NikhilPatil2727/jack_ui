b55049968c5ed19999712af5b46ff0ad37f0e5f9
NikhilPatil2727 <nikhilpatil02436@gmail.com>
2026-05-16 19:41:56 +0530

    Add new icons and components for UI features
    - Created new SVG icons: Motion, Next.js, React, Shadcn, TailwindCSS, v0, and XIcon.
    - Implemented feature block component to display icons with hover effects.
    - Developed hero section with introductory text and buttons for browsing components and blocks.
    - Added browse button components for navigating to templates and components.
    - Introduced a custom hook for auto-resizing text areas.

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,12 @@
+import HeroSection from "@/components/landing/hero";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
-      
+       <main className="bg-white dark:bg-black/5 overflow-x-hidden">
+         <HeroSection />
+       </main>
     </div>
   );
 }
