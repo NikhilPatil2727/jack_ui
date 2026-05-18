a250977ac239d8c0abfe4c37417158a9d0927524
NikhilPatil2727 <nikhilpatil02436@gmail.com>
2026-05-18 10:06:34 +0530

    feat: enhance layout with navigation links and logo integration

--- lib/layout.shared.tsx
+++ lib/layout.shared.tsx
@@ -1,9 +1,23 @@
-import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared';
-
+import type { BaseLayoutProps } from "fumadocs-ui/layouts/shared";
+import { Flame } from "lucide-react";
+import Image from "next/image";
 export function baseOptions(): BaseLayoutProps {
   return {
     nav: {
-      title: 'My App',
+      title: (
+        <div className="flex items-center">
+           <Image
+                                src="/image.png"
+                                alt="Jack UI logo"
+                                width={40}
+                                height={40}
+                                className="h-10 w-10 rounded-md"
+                              />
+          <span className="hidden md:inline-flex items-center text-lg font-bold tracking-tight text-black dark:text-white">
+            JackUI
+          </span>
+        </div>
+      ),
     },
     themeSwitch: {
       enabled: false,
