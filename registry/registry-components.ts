b5a8aca75cad60565afcd9669b2c2d9a60e4dc75
NikhilPatil2727 <nikhilpatil02436@gmail.com>
2026-07-05 18:21:14 +0530

    (feat):delete unwanted Registry and mdx files

--- registry/registry-components.ts
+++ registry/registry-components.ts
@@ -1,22 +1,22 @@
 import type { Registry } from "./schema";
 
 export const component: Registry = [
-    {
-        name: "ai-input-01",
-        type: "registry:component",
-        dependencies: ["lucide-react"],
-        registryDependencies: ["textarea"],
-        files: [
-            {
-                path: "components/jackui/ai-input/ai-input-01.tsx",
-                type: "registry:component",
-            },
-            {
-                path: "hooks/use-auto-resize-textarea.ts",
-                type: "registry:hook",
-            },
-        ],
-    },
+    // {
+    //     name: "ai-input-01",
+    //     type: "registry:component",
+    //     dependencies: ["lucide-react"],
+    //     registryDependencies: ["textarea"],
+    //     files: [
+    //         {
+    //             path: "components/jackui/ai-input/ai-input-01.tsx",
+    //             type: "registry:component",
+    //         },
+    //         {
+    //             path: "hooks/use-auto-resize-textarea.ts",
+    //             type: "registry:hook",
+    //         },
+    //     ],
+    // },
     {
         name: "InkFillBtn",
         type: "registry:component",
@@ -62,28 +62,28 @@ export const component: Registry = [
             },
         ],
     },
-    {
-        name: "btn-05",
-        type: "registry:component",
-        dependencies: ["motion"],
-        files: [
-            {
-                path: "components/jackui/buttons/btn-05.tsx",
-                type: "registry:component",
-            },
-        ],
-    },
-    {
-        name: "btn-06",
-        type: "registry:component",
-        dependencies: ["motion"],
-        files: [
-            {
-                path: "components/jackui/buttons/btn-06.tsx",
-                type: "registry:component",
-            },
-        ],
-    },
+    // {
+    //     name: "btn-05",
+    //     type: "registry:component",
+    //     dependencies: ["motion"],
+    //     files: [
+    //         {
+    //             path: "components/jackui/buttons/btn-05.tsx",
+    //             type: "registry:component",
+    //         },
+    //     ],
+    // },
+    // {
+    //     name: "btn-06",
+    //     type: "registry:component",
+    //     dependencies: ["motion"],
+    //     files: [
+    //         {
+    //             path: "components/jackui/buttons/btn-06.tsx",
+    //             type: "registry:component",
+    //         },
+    //     ],
+    // },
     {
         name: "star-burst-button",
         type: "registry:component",
@@ -95,99 +95,99 @@ export const component: Registry = [
             },
         ],
     },
-    {
-        name: "btn-08",
-        type: "registry:component",
-        dependencies: ["lucide-react"],
-        registryDependencies: ["button"],
-        files: [
-            {
-                path: "components/jackui/buttons/btn-08.tsx",
-                type: "registry:component",
-            },
-        ],
-    },
-    {
-        name: "btn-09",
-        type: "registry:component",
-        registryDependencies: ["button"],
-        files: [
-            {
-                path: "components/jackui/buttons/btn-09.tsx",
-                type: "registry:component",
-            },
-        ],
-    },
-    {
-        name: "btn-10",
-        type: "registry:component",
-        dependencies: ["lucide-react"],
-        registryDependencies: ["button"],
-        files: [
-            {
-                path: "components/jackui/buttons/btn-10.tsx",
-                type: "registry:component",
-            },
-        ],
-    },
-    {
-        name: "btn-11",
-        type: "registry:component",
-        dependencies: ["lucide-react"],
-        registryDependencies: ["button"],
-        files: [
-            {
-                path: "components/jackui/buttons/btn-11.tsx",
-                type: "registry:component",
-            },
-        ],
-    },
-    {
-        name: "btn-12",
-        type: "registry:component",
-        registryDependencies: ["button"],
-        files: [
-            {
-                path: "components/jackui/buttons/btn-12.tsx",
-                type: "registry:component",
-            },
-        ],
-    },
-    {
-        name: "btn-13",
-        type: "registry:component",
-        registryDependencies: ["button"],
-        files: [
-            {
-                path: "components/jackui/buttons/btn-13.tsx",
-                type: "registry:component",
-            },
-        ],
-    },
-    {
-        name: "btn-14",
-        type: "registry:component",
-        dependencies: ["lucide-react"],
-        registryDependencies: ["button"],
-        files: [
-            {
-                path: "components/jackui/buttons/btn-14.tsx",
-                type: "registry:component",
-            },
-        ],
-    },
-    {
-        name: "btn-15",
-        type: "registry:component",
-        dependencies: ["lucide-react"],
-        registryDependencies: ["button"],
-        files: [
-            {
-                path: "components/jackui/buttons/btn-15.tsx",
-                type: "registry:component",
-            },
-        ],
-    },
+    // {
+    //     name: "btn-08",
+    //     type: "registry:component",
+    //     dependencies: ["lucide-react"],
+    //     registryDependencies: ["button"],
+    //     files: [
+    //         {
+    //             path: "components/jackui/buttons/btn-08.tsx",
+    //             type: "registry:component",
+    //         },
+    //     ],
+    // },
+    // {
+    //     name: "btn-09",
+    //     type: "registry:component",
+    //     registryDependencies: ["button"],
+    //     files: [
+    //         {
+    //             path: "components/jackui/buttons/btn-09.tsx",
+    //             type: "registry:component",
+    //         },
+    //     ],
+    // },
+    // {
+    //     name: "btn-10",
+    //     type: "registry:component",
+    //     dependencies: ["lucide-react"],
+    //     registryDependencies: ["button"],
+    //     files: [
+    //         {
+    //             path: "components/jackui/buttons/btn-10.tsx",
+    //             type: "registry:component",
+    //         },
+    //     ],
+    // },
+    // {
+    //     name: "btn-11",
+    //     type: "registry:component",
+    //     dependencies: ["lucide-react"],
+    //     registryDependencies: ["button"],
+    //     files: [
+    //         {
+    //             path: "components/jackui/buttons/btn-11.tsx",
+    //             type: "registry:component",
+    //         },
+    //     ],
+    // },
+    // {
+    //     name: "btn-12",
+    //     type: "registry:component",
+    //     registryDependencies: ["button"],
+    //     files: [
+    //         {
+    //             path: "components/jackui/buttons/btn-12.tsx",
+    //             type: "registry:component",
+    //         },
+    //     ],
+    // },
+    // {
+    //     name: "btn-13",
+    //     type: "registry:component",
+    //     registryDependencies: ["button"],
+    //     files: [
+    //         {
+    //             path: "components/jackui/buttons/btn-13.tsx",
+    //             type: "registry:component",
+    //         },
+    //     ],
+    // },
+    // {
+    //     name: "btn-14",
+    //     type: "registry:component",
+    //     dependencies: ["lucide-react"],
+    //     registryDependencies: ["button"],
+    //     files: [
+    //         {
+    //             path: "components/jackui/buttons/btn-14.tsx",
+    //             type: "registry:component",
+    //         },
+    //     ],
+    // },
+    // {
+    //     name: "btn-15",
+    //     type: "registry:component",
+    //     dependencies: ["lucide-react"],
+    //     registryDependencies: ["button"],
+    //     files: [
+    //         {
+    //             path: "components/jackui/buttons/btn-15.tsx",
+    //             type: "registry:component",
+    //         },
+    //     ],
+    // },
     {
         name: "envelope-card",
         type: "registry:component",
